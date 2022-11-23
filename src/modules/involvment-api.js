0fdd748cb3c3a9acf4c8cf0fde348be2d0e64dc7
Luis Muñoz <lmunozm1702@gmail.com>
2022-11-23 12:50:57 -0300

    Add function to set a like for a book

--- src/modules/involvment-api.js
+++ src/modules/involvment-api.js
@@ -22,4 +22,27 @@ const getAppLikes = async () => {
   return (false);
 };
 
+const setAppLike = async () => {
+  /*
+  const response = await fetch(`${API_URL}/apps/${APP_ID}/likes/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'access-control-allow-origin': '*',
+      'Access-Control-Allow-Credentials': 'true',
+    },
+  });
+  if (response.ok) {
+    try {
+      const result = await response.json();
+      return (result);
+    } catch (e) {
+      const result = [];
+      return (result);
+    }
+  }
+  return (false);
+*/
+}
+
 export default getAppLikes;
